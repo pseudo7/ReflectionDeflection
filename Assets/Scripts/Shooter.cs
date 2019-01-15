e5a651bca98678b6a4868ed634a18fde762af913
Pseudo <enikhil7@gmail.com>
2019-01-16 01:18:20 +0530

    Added Gun

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,11 +7,18 @@ public class Shooter : MonoBehaviour
     public static Shooter Instance;
 
     [SerializeField] Transform player;
+    [SerializeField] Transform gun;
     [SerializeField] GameObject ballPrefab;
+    [SerializeField] float gunRecoilSpeed = 2f;
     [SerializeField] int shootingSpeed = 2;
     [SerializeField] int destroyDelay = 10;
+
+    readonly Vector3 gunShootPos = new Vector3(0f, -.15f, 0f);
+    readonly Vector3 gunRecoilPos = new Vector3(0f, -.3f, -.01f);
     Transform mainCamTransform;
+    Vector3 gunOrigPos;
     Camera mainCam;
+    bool recoiling;
 
     private void Awake()
     {
@@ -23,6 +30,7 @@ public class Shooter : MonoBehaviour
     {
         mainCam = Camera.main;
         mainCamTransform = mainCam.transform;
+        gunOrigPos = gun.localPosition;
     }
 
 #if UNITY_EDITOR
@@ -40,6 +48,9 @@ public class Shooter : MonoBehaviour
         if (Physics.Raycast(player.position, Camera.main.transform.forward, out hit))
             if (hit.collider.CompareTag(Constants.MIRROR_TAG))
             {
+                if (recoiling)
+                    return;
+                StartCoroutine(Recoiling());
                 var ball = Instantiate(ballPrefab, mainCamTransform.position, Quaternion.identity);
                 ball.GetComponent<Ball>().initialVelocity = (hit.transform.position - mainCamTransform.position).normalized * shootingSpeed;
                 ball.SetActive(true);
@@ -47,5 +58,30 @@ public class Shooter : MonoBehaviour
             }
             else Debug.LogWarning("TAG: " + hit.collider.tag);
         else Debug.Log("Nothing to Shoot");
+    }
+
+    IEnumerator Recoiling()
+    {
+        recoiling = true;
+
+        while (gun.localPosition != gunShootPos)
+        {
+            gun.localPosition = Vector3.MoveTowards(gun.localPosition, gunShootPos, Time.deltaTime * gunRecoilSpeed * 5);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForEndOfFrame();
+        while (gun.localPosition != gunRecoilPos)
+        {
+            gun.localPosition = Vector3.MoveTowards(gun.localPosition, gunRecoilPos, Time.deltaTime * gunRecoilSpeed * 2);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForEndOfFrame();
+        while (gun.localPosition != gunOrigPos)
+        {
+            gun.localPosition = Vector3.MoveTowards(gun.localPosition, gunOrigPos, Time.deltaTime * gunRecoilSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+
+        recoiling = false;
     }
 }
